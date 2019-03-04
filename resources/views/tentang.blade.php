fe793336bea7748e4f6a4e52fa7bd6c9196cfab8
reginagustiarti <reginagustiarti@gmail.com>
2019-03-04 08:21:32 +0700

    first commit

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,20 @@
+<!-- Menghubungkan dengan view template master -->
+@extends('master')
+ 
+<!-- isi bagian judul halaman -->
+<!-- cara penulisan isi section yang pendek -->
+@section('judul_halaman', 'Halaman Tentang')
+ 
+ 
+<!-- isi bagian konten -->
+<!-- cara penulisan isi section yang panjang -->
+@section('konten')
+ 
+	<p>Ini Adalah Halaman Tentang</p>
+	<p>
+        Hay Nama saya REGINA GUSTIARTI saya sekolah di SMKN 1 Subang,ini blog pertama saya dan masih ngikutin tutorial. 
+        duduk dikelas XI dan mengambil jurusan RPL. Saya anak ke satu dari dua bersaudara, saya lahir pada tanggal 12 agustus 2002.
+        Saya berumur 16 tahun, thankyou 
+	</p>
+ 
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//route bawaan dealfut laravel
+Route::get('/', function () {
+    return view('welcome'); 
+});
+
+// route blog
+Route::get('/blog', 'BlogController@home');
+Route::get('/blog/tentang', 'BlogController@tentang');
+Route::get('/blog/kontak', 'BlogController@kontak');
+
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+
+Route::post('/pegawai/store','PegawaiController@store');
+
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+
+Route::post('/pegawai/update','PegawaiController@update');
+
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
+
